1c5f2b6ec489fc2c689c25f73f8d14214dfeae88
root <professionaleditor6@gmail.com>
2022-12-21 13:52:52 +0000

    add files of tasks

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const successObject = {
+        status: 200,
+        body: 'Success',
+      };
+      resolve(successObject);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
