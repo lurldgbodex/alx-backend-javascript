aa28dbdc9eeddd29673311dac5b6bc56c2a06aae
root <professionaleditor6@gmail.com>
2023-02-22 14:04:58 +0000

    add files of task

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const port = 1245;
+const hostname = 'localhost';
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
